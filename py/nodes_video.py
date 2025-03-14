b06e00b985f46e94528416dce7895b9d2fe7b34a
zhangsongbo <zhangp365@gmail.com>
2025-03-14 11:49:11 +0800

    update the FrameAdjuster node.

--- py/nodes_video.py
+++ py/nodes_video.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import logging
+logger = logging.getLogger(__name__)
 
 class FrameAdjuster:
     def __init__(self):
@@ -12,6 +14,9 @@ class FrameAdjuster:
             "duration": ("FLOAT", {"default": 5.0, "min": 0.1, "max": 60.0, "step": 0.1}),
             "fps": ("FLOAT", {"default": 24.0, "min": 1.0, "max": 120.0, "step": 1.0}),
             "remove_frames": ("INT", {"default": 0, "min": 0, "max": 20, "step": 1}),
+            },
+            "optional": {
+                "extend_tail_frame_if_adjust":("BOOLEAN", {"default": False})
             }
         }
     
@@ -20,7 +25,7 @@ class FrameAdjuster:
     FUNCTION = "adjust_frames"
     CATEGORY = "utils"
 
-    def adjust_frames(self, images, duration, fps, remove_frames):
+    def adjust_frames(self, images: torch.Tensor, duration: float, fps: float, remove_frames: int, extend_tail_frame_if_adjust: bool = False):
         if remove_frames > 0:
             images = images[:-remove_frames]
         batch_size = images.shape[0]
@@ -33,7 +38,7 @@ class FrameAdjuster:
         
         # 如果帧数过少，需要插值
         if batch_size < min_frames:
-            target_frames = min_frames + 5
+            target_frames = min_frames + 5  if not extend_tail_frame_if_adjust else min_frames
 
         # 如果帧数过多，需要减帧
         if batch_size > max_frames:
@@ -41,6 +46,13 @@ class FrameAdjuster:
         indices = np.linspace(0, batch_size - 1, target_frames)
         indices = np.floor(indices).astype(int)
         new_images = images[indices]
+
+
+        if extend_tail_frame_if_adjust:
+            unique, counts = np.unique(indices, return_counts=True)
+            repeat_count = np.min(counts[:-1]) if len(counts) > 1 else int(fps // 2)
+            logger.info(f"repeat_count: {repeat_count}, unique: {unique}, counts: {counts}")
+            new_images = torch.cat([new_images, images[-1].unsqueeze(0).repeat(repeat_count, 1, 1, 1)], dim=0)
         return (new_images, len(new_images), fps)
 
 NODE_CLASS_MAPPINGS = {
